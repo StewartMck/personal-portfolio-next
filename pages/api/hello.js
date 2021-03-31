92e51fdc42d8e8ca86c9f16f3579c5d7041e0c52
Stewart <stewart.mckinlay@gmail.com>
2021-03-31 10:17:41 -0600

    Setup prisma client

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {PrismaClient} from '@prisma/client'
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+const prisma = new PrismaClient()
+
+export default async (req, res) => {
+  const data = await prisma.project.findMany()
+
+  res.status(200).json({ name: data })
 }
