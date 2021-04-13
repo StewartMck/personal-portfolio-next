819e6812bd171ac0acf1ff7c1b270ebbb2bed5e1
Stewart <stewart.mckinlay@gmail.com>
2021-04-13 13:13:43 -0600

    Fix db issues

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient
-}
+const prisma = new PrismaClient();
+// declare global {
+//   var prisma: PrismaClient
+// }
 
-let prisma;
+// let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
 
 export default prisma;
